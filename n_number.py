8119c43d665ad45297f3bc34ac3538439942e93b
Aayursha more <aayurshamore22@gmail.com>
2026-06-27 20:04:20 +0530

    Add files via upload

---
+++ n_number.py
@@ -0,0 +1,6 @@
+n = int(input("entr a number: "))
+total = 0 
+for i in range (1, n, 1):
+    total += i 
+    print("sum =", total)
+    
---
+++ vowel_or_consonant.py
@@ -0,0 +1,7 @@
+letter = input("Enter an alphabet: ").lower()
+ 
+if letter in "aeiou":
+ print("Vowel")
+else:
+ print("consonant")
+ 
