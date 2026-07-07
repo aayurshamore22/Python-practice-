682ec1178c7765802c743fc4bc1dc2c95e85dcfc
Aayursha more <aayurshamore22@gmail.com>
2026-07-07 22:57:15 +0530

    Delete operator directory

--- operator/calculator.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("enter first number: "))
-num2 = int(input("enter second number: "))
-
-print("addition =", num1 + num2)
-print("subtraction =", num1 - num2)
-print("multiplication =", num1 * num2)
-print("division =", num1 / num2)
-
